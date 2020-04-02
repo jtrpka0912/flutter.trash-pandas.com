88c67a45c8c6a63759a5a65ab879f39af8ce85d9
J. Trpka <jtrpka0912@gmail.com>
2020-04-02 15:33:58 -0400

    Completed code for scoring points to players.

--- lib/models/trash_panda_data.dart
+++ lib/models/trash_panda_data.dart
@@ -9,32 +9,35 @@ class TrashPandaData extends ChangeNotifier {
     return _players.length;
   }
 
-  void addPlayer(Player player) {
-    // Check if no more than four players
-    if(playerCount < 4) {
-      _players.add(player);
-    } else {
-      throw Exception('You can not have more than four players.');
+  void addPlayers(int howManyPlayers) {
+    _players.clear(); // Reset the player listing
+
+    if(howManyPlayers > 1 && howManyPlayers < 5) {
+      for(int x = 0; x < howManyPlayers; x++) {
+        _players.add(Player()); // Add an empty player
+      }
+    } else if(howManyPlayers > 4) {
+      throw Exception('You can not add more than four players.');
+    } else if(howManyPlayers < 2) {
+      throw Exception('You must have at least two players.');
     }
   }
 
-  // TODO: Apply the tally methods here
-  // ==================================
-  // TODO: Apply Shiny Count
-  void applyShinyCount() {
-    _addCardPoints(
-        players: _players,
-        card: Card.Shiny,
-        firstPlacePoints: 3,
-        secondPlacePoints: 0,
-        thirdPlacePoints: 0
-    );
+  Player getPlayer(int playerIndex) {
+    try {
+      return _players[playerIndex];
+    } catch(Exception) {
+      // TODO: May want to do something with the exception or throw one.
+      return null;
+    }
   }
-  // TODO: Apply YumYum Count
-  // TODO: Apply Feesh Count
-  // TODO: Apply MmmPie Count
-  // TODO: Apply Nanners Count
-  // TODO: Apply Blammo Count
+
+  void applyShinyCount() => _addCardPoints(Card.Shiny, 3, 0, 0);
+  void applyYumYumCount() => _addCardPoints(Card.YumYum, 4, 2, 0);
+  void applyFeeshCount() => _addCardPoints(Card.Feesh, 5, 3, 1);
+  void applyMmmPieCount() => _addCardPoints(Card.MmmPie, 6, 2, 1);
+  void applyNannersCount() => _addCardPoints(Card.Nanners, 7, 0, 0);
+
   void applyBlammoCount() {
     for(Player player in _players) {
       player.increaseScore = player.getCardCount(Card.Blammo);
@@ -42,26 +45,104 @@ class TrashPandaData extends ChangeNotifier {
   }
 
   // TODO: Make the method to check which player had the most of a card
-  void _addCardPoints({
-    List<Player> players,
+  void _addCardPoints(
     Card card,
     int firstPlacePoints,
     int secondPlacePoints,
     int thirdPlacePoints
-  }) {
+  ) {
+    for(Player mainPlayer in _players) {
+      mainPlayer.increaseScore = _playerPosition(mainPlayer, card, firstPlacePoints, secondPlacePoints, thirdPlacePoints);
+    }
+  }
 
+  int _playerPosition(Player mainPlayer, Card card, int firstPlacePoints, int secondPlacePoints, int thirdPlacePoints) {
+    /// Pick main player
+    /// Loop through other players to see if any ties
+    ///   Yes: Add player index to a list or flagged for tie
+    ///   No: Check if higher than player
+    ///     Yes: Increment how many players main player is higher
+    ///     No: Continue
+    /// Once loop is done; determine how many points player(s) get
+    /// This is determined by player count
+    /// If four players, and main player beats (based on increment):
+    ///   0 players: fourth place
+    ///   1 player: third place
+    ///   2 players: second place
+    ///   3 players: first place
+    ///   If flagged for tie; decrease one point
+    ///   Four Players: 0: 4th, 1: 3rd, 2: 2nd, 3: 1st
+    ///   Three Players: 0: 3rd, 1: 2nd, 2: 1st
+    ///   Two Players: 0: 2nd, 1: 1st
+    ///
+    List<Player> otherPlayers = List.from(_players);
+    otherPlayers.remove(mainPlayer); // Remove the main player from list
+    bool isTied = false;
+    int higherThanPlayers = 0;
+
+    for(Player otherPlayer in otherPlayers) {
+      int mainPlayerCardCount = mainPlayer.getCardCount(card);
+      int otherPlayerCardCount = otherPlayer.getCardCount(card);
+
+      // Check if not already tied and matches other players' card count
+      if(!isTied && (mainPlayerCardCount == otherPlayerCardCount)) {
+        isTied = true;
+        higherThanPlayers++;
+      }
+
+      // Check if player has higher card count than other player
+      if(mainPlayerCardCount > otherPlayerCardCount) {
+        higherThanPlayers++;
+      }
+    }
+
+    // Get the number of points based on how many players the main player beat
+    switch(otherPlayers.length + 1) {
+      case 2:
+        switch(higherThanPlayers) {
+          case 1:
+            return isTied ? firstPlacePoints - 1 : firstPlacePoints;
+          default:
+            return secondPlacePoints; // No need to check for tie
+        }
+        break;
+      case 3:
+        switch(higherThanPlayers) {
+          case 2:
+            return isTied ? firstPlacePoints - 1 : firstPlacePoints;
+          case 1:
+            return isTied ? secondPlacePoints - 1 : secondPlacePoints;
+          default:
+            return thirdPlacePoints; // No need to check for tie
+        }
+        break;
+      case 4:
+        switch(higherThanPlayers) {
+          case 3:
+            return isTied ? firstPlacePoints - 1 : firstPlacePoints;
+          case 2:
+            return isTied ? secondPlacePoints - 1 : secondPlacePoints;
+          case 1:
+            return isTied ? thirdPlacePoints - 1 : thirdPlacePoints;
+          default:
+            return 0;
+        }
+    }
+    // Something happened
+    // TODO: Should encase inside try/catch block
+    return 0;
   }
 }
 
 class Player {
   bool _active = false;
   String _name;
-  Map<Card, int> _cards;
+  Map<Card, int> _cards = {};
   int _score = 0;
 
-  bool get isActive {
-    return _active;
-  }
+  bool get isActive => _active;
+  String get name => _name;
+  int get score => _score;
 
   int getCardCount(Card cardName) {
     return _cards[cardName];
--- test/models/trash_panda_data_test.dart
+++ test/models/trash_panda_data_test.dart
@@ -2,30 +2,122 @@ import 'package:flutter_test/flutter_test.dart';
 import 'package:trashpandas/models/trash_panda_data.dart';
 
 void main() {
-
   group('Player Tests', () {
     test('Add two players; player count should expect that much.', () {
       // TODO: Need to figure out how to set this before each test to make it DRY.
       TrashPandaData data = TrashPandaData();
 
-      data.addPlayer(Player());
-      data.addPlayer(Player());
+      data.addPlayers(2);
 
       expect(data.playerCount, 2);
     });
 
-    test('Fail after adding a fifth player.', () {
+    test('Reset player listing from three to four.', () {
       TrashPandaData data = TrashPandaData();
 
-      data.addPlayer(Player());
-      data.addPlayer(Player());
-      data.addPlayer(Player());
-      data.addPlayer(Player());
+      data.addPlayers(3);
+      expect(data.playerCount, 3);
 
-      expect(() => data.addPlayer(Player()), throwsException);
+      data.addPlayers(4);
+      expect(data.playerCount, 4);
+    });
+
+    test('Fail trying to add five players.', () {
+      TrashPandaData data = TrashPandaData();
+      expect(() => data.addPlayers(5), throwsException);
+    });
+
+    test('Fail trying to add only one player.', () {
+      TrashPandaData data = TrashPandaData();
+      expect(() => data.addPlayers(1), throwsException);
+    });
+
+    test('Retrieve a player', () {
+      TrashPandaData data = TrashPandaData();
+
+      data.addPlayers(3);
+      expect(data.playerCount, 3); // Test if player count matches
+
+      Player playerTwo = data.getPlayer(1);
+      expect(data.getPlayer(1), playerTwo);
+      expect(data.getPlayer(2), isA<Player>());
+    });
+
+    test('Unable to retrieve a player.', () {
+      TrashPandaData data = TrashPandaData();
+
+      data.addPlayers(2);
+
+      expect(data.getPlayer(2), null);
     });
   });
-  group('Score Tests', () {
 
+  group('Score Tests', () {
+    test('Check if player blammo cards add up to scores.', () {
+      TrashPandaData data = TrashPandaData();
+      data.addPlayers(2);
+      expect(data.playerCount, 2);
+
+      Player playerOne = data.getPlayer(0);
+      playerOne.setCardCount(Card.Blammo, 3);
+      expect(playerOne.getCardCount(Card.Blammo), 3);
+
+      Player playerTwo = data.getPlayer(1);
+      playerTwo.setCardCount(Card.Blammo, 5);
+      expect(playerTwo.getCardCount(Card.Blammo), 5);
+
+      data.applyBlammoCount();
+
+      expect(playerOne.score, 3);
+      expect(playerTwo.score, 5);
+    });
+
+    test('Check who got points from Feesh', () {
+      TrashPandaData data = TrashPandaData();
+      data.addPlayers(3);
+      expect(data.playerCount, 3);
+
+      Player playerOne = data.getPlayer(0);
+      playerOne.setCardCount(Card.Feesh, 3);
+      expect(playerOne.getCardCount(Card.Feesh), 3);
+
+      Player playerTwo = data.getPlayer(1);
+      playerTwo.setCardCount(Card.Feesh, 5);
+      expect(playerTwo.getCardCount(Card.Feesh), 5);
+
+      Player playerThree = data.getPlayer(2);
+      playerThree.setCardCount(Card.Feesh, 2);
+      expect(playerThree.getCardCount(Card.Feesh), 2);
+
+      data.applyFeeshCount();
+
+      expect(playerOne.score, 3);
+      expect(playerTwo.score, 5);
+      expect(playerThree.score, 1);
+    });
+
+    test('Check who got points, with a tie, from YumYum', () {
+      TrashPandaData data = TrashPandaData();
+      data.addPlayers(3);
+      expect(data.playerCount, 3);
+
+      Player playerOne = data.getPlayer(0);
+      playerOne.setCardCount(Card.YumYum, 1);
+      expect(playerOne.getCardCount(Card.YumYum), 1);
+
+      Player playerTwo = data.getPlayer(1);
+      playerTwo.setCardCount(Card.YumYum, 2);
+      expect(playerTwo.getCardCount(Card.YumYum), 2);
+
+      Player playerThree = data.getPlayer(2);
+      playerThree.setCardCount(Card.YumYum, 1);
+      expect(playerThree.getCardCount(Card.YumYum), 1);
+
+      data.applyYumYumCount();
+
+      expect(playerOne.score, 1); // Gets one less (1 from 2) from tie
+      expect(playerTwo.score, 4);
+      expect(playerThree.score, 1); // Gets one less (1 from 2) from tie
+    });
   });
 }
