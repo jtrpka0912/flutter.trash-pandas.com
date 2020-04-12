0c59d9cf52e4d3f30d58c40da4b13238353934e4
J. Trpka <jtrpka0912@gmail.com>
2020-04-11 21:07:55 -0400

    Added functionality to form field.

--- lib/models/player.dart
+++ lib/models/player.dart
@@ -1,18 +1,26 @@
 // Models
+import 'package:flutter/material.dart';
 import 'package:trashpandas/models/enums.dart';
 
 class Player {
   String name;
-  Map<Card, int> _cards = {};
+  Map<CardNames, int> _cards = {
+    CardNames.Shiny: 0,
+    CardNames.YumYum: 0,
+    CardNames.Nanners: 0,
+    CardNames.MmmPie: 0,
+    CardNames.Feesh: 0,
+    CardNames.Blammo: 0,
+  };
   int _score = 0;
 
   int get score => _score;
 
-  int getCardCount(Card cardName) {
+  int getCardCount(CardNames cardName) {
     return _cards[cardName];
   }
 
-  void setCardCount(Card cardName, int count) {
+  void setCardCount(CardNames cardName, int count) {
     _cards[cardName] = count;
   }
 
--- lib/scenes/card_count.dart
+++ lib/scenes/card_count.dart
@@ -5,9 +5,12 @@ import 'package:provider/provider.dart';
 // Models
 import 'package:trashpandas/models/trash_panda_data.dart';
 import 'package:trashpandas/models/player.dart';
+import 'package:trashpandas/models/enums.dart';
 
 class CardCountScene extends StatelessWidget {
   static const routeName = '/cards';
+
+  final _cardCountKey = GlobalKey<FormState>();
 
   String nextOrFinal(int playerIndex, int playerCount) {
     return morePlayersAhead(playerIndex, playerCount) ? 'Next' : 'Final Tally';
@@ -26,32 +29,101 @@ class CardCountScene extends StatelessWidget {
         Player player = trashPandaData.getPlayer(playerIndex);
 
         return Scaffold(
+          // TODO: Make a separate widget for the AppBar
           appBar: AppBar(
             title: Text('Card Count for ${player.name}'),
             centerTitle: true,
           ),
-          body: Container(),
+          body: Container(
+            child: Form(
+              key: _cardCountKey,
+              child: ListView(
+                children: [
+                  Container(
+                    padding: EdgeInsets.only(bottom: 36.0),
+                    child: TextFormField(
+                      keyboardType: TextInputType.number,
+                      initialValue: trashPandaData
+                        .getPlayer(playerIndex)
+                        .getCardCount(CardNames.Shiny)
+                        .toString(),
+                      validator: (String numberOfCardsString) {
+                        final int numberOfCards = int.tryParse(numberOfCardsString) ?? 0;
+
+                        if(numberOfCards < 0) {
+                          return 'You can not have negative cards!';
+                        }
+
+                        CardNames cardName = CardNames.Shiny;
+                        switch(cardName) {
+                          case CardNames.Shiny:
+                            if(numberOfCards > 3) {
+                              return 'You can not have more than three Shiny cards.';
+                            }
+                            break;
+                          case CardNames.YumYum:
+                            if(numberOfCards > 5) {
+                              return 'You can not have more than five Yum Yum cards.';
+                            }
+                            break;
+                          case CardNames.Feesh:
+                            if(numberOfCards > 7) {
+                              return 'You can not have more than seven Feesh cards.';
+                            }
+                            break;
+                          case CardNames.MmmPie:
+                            if(numberOfCards > 9) {
+                              return 'You can not have more than nine Mmm Pie cards.';
+                            }
+                            break;
+                          case CardNames.Nanners:
+                            if(numberOfCards > 11) {
+                              return 'You can not have more than eleven Nanner cards.';
+                            }
+                            break;
+                          case CardNames.Blammo:
+                            if(numberOfCards > 13) {
+                              return 'You can not have more than thirteen Blammo cards.';
+                            }
+                            break;
+                        }
+
+                        return null;
+                      },
+                      onChanged: (String numberOfCardsString) {
+                        final int numberOfCards = int.tryParse(numberOfCardsString) ?? 0;
+                        trashPandaData
+                          .getPlayer(playerIndex)
+                          .setCardCount(CardNames.Shiny, numberOfCards);
+                      },
+                    )
+                  )
+                ]
+              )
+            )
+          ),
           bottomNavigationBar: BottomAppBar(
             child: Builder(
               builder: (context) => FlatButton(
                 color: Theme.of(context).bottomAppBarTheme.color,
-                // TODO: Either Next or Final Score depending if
-                // playerIndex + 1 equals playerCount
                 child: Text(nextOrFinal(playerIndex, trashPandaData.playerCount)),
                 onPressed: () {
-                  // TODO: Check if playerIndex + 1 is less than playerCount
-                  // If less: go to next player
+                  if(_cardCountKey.currentState.validate()) {
+                    print('I got ${trashPandaData.getPlayer(playerIndex).getCardCount(CardNames.Shiny)} ${CardNames.Shiny} cards.');
 
-                  if(morePlayersAhead(playerIndex, trashPandaData.playerCount)) {
-                    Navigator.pushNamed(
-                      context,
-                      CardCountScene.routeName,
-                      arguments: playerIndex + 1
-                    );
-                  } else {
-                    // Else: go to final tally
-                    print('Go to final score page');
+                    if(morePlayersAhead(playerIndex, trashPandaData.playerCount)) {
+                      // Go to next player
+                      Navigator.pushNamed(
+                          context,
+                          CardCountScene.routeName,
+                          arguments: playerIndex + 1
+                      );
+                    } else {
+                      // Else: go to final tally
+                      print('Go to final score page');
+                    }
                   }
+
                 },
               )
             ),
