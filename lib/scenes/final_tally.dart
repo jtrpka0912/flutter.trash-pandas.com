c99f0c06366be8b95839d90e96b03bc4f45b6c20
J. Trpka <jtrpka0912@gmail.com>
2020-04-14 07:39:45 -0400

    Initial build of the bottom sheet for icon buttons.

--- lib/scenes/final_tally.dart
+++ lib/scenes/final_tally.dart
@@ -17,25 +17,6 @@ import 'package:trashpandas/scenes/player_count.dart';
 class FinalTallyScene extends StatelessWidget {
   static const routeName = '/final';
   
-  List<Text> printPlacementOrder(TrashPandaData trashPandaData) {
-    List<Text> textWidgets = [];
-    int iterator = 1;
-    for(Player player in trashPandaData.getFinalPlayerTallyPlacement()) {
-      Text playerText = Text('${iterator.toString()}. ${player.name} (${player.score})');
-      textWidgets.add(playerText);
-      textWidgets.add(Text('================'));
-
-      for(CardNames card in CardNames.values) {
-        Text scoreText = Text('${card.toString()}: ${player.getCardScore(card).toString()} from ${player.getCardCount(card).toString()} cards');
-        textWidgets.add(scoreText);
-      }
-
-      iterator++;
-    }
-    
-    return textWidgets;
-  }
-
   Widget firstPlayerWidget(TrashPandaData trashPandaData) {
     List<Player> players = trashPandaData.getFinalPlayerTallyPlacement();
     Player firstPlacePlayer = players[0];
@@ -123,6 +104,11 @@ class FinalTallyScene extends StatelessWidget {
               SizedBox(height: 36.0),
               otherPlayerWidget(trashPandaData)
             ]
+          ),
+          bottomSheet: BottomSheet(
+            builder: (BuildContext context) {
+              return Column();
+            },
           ),
           bottomNavigationBar: BottomAppBar(
             child: Builder(
