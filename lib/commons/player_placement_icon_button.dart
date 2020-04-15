01ac8036694205cd92ad47bdd278e6c714620e6a
J. Trpka <jtrpka0912@gmail.com>
2020-04-14 20:39:26 -0400

    Created card detail tile widget.

--- lib/commons/player_placement_icon_button.dart
+++ lib/commons/player_placement_icon_button.dart
@@ -1,89 +1,61 @@
 // Packages
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:trashpandas/commons/card_detail_tile.dart';
+
+// Commons
+import 'package:trashpandas/commons/card_detail_tile.dart';
 
 // Models
 import 'package:trashpandas/models/trash_panda_data.dart';
 import 'package:trashpandas/models/player.dart';
 import 'package:trashpandas/models/enums.dart';
 
-// Helpers
-import 'package:trashpandas/helper_functions.dart';
-
 class PlayerPlacementIconButton extends StatelessWidget {
   final Player player;
 
   PlayerPlacementIconButton({this.player});
 
-  Widget buildBottomSheet(BuildContext context) {
+  Container buildBottomSheet(BuildContext context) {
     return Container(
-      child: ListView(
-        children: [
-          cardCountAndPoints(context, player)
-        ],
-      )
+      child: cardCountAndPoints(context, player),
     );
   }
 
   Row cardCountAndPoints(BuildContext context, Player player) {
-    List<Widget> left = [];
-    List<Widget> right = [];
-    
+    List<CardDetailTile> left = [];
+    List<CardDetailTile> right = [];
+
     int iterator = 0;
     for(CardNames card in CardNames.values) {
-      ListTile cardDetail = ListTile(
-        leading: Container(
-          // TODO: Change color when text field is focused
-          padding: EdgeInsets.all(4.0),
-          decoration: BoxDecoration(
-            color: Theme.of(context).primaryColor,
-            border: Border.all(
-              color: Colors.black45,
-              width: 2.0
-            )
-          ),
-          child: Image.asset(
-            'assets/images/${getCardName(card).toLowerCase()}.png',
-            width: 25
-          ),
-        ),
-        title: Text('${getCardName(card)}'),
-        subtitle: Row(
-          children: <Widget>[
-            Text('Count: ${player.getCardCount(card)}'),
-            SizedBox(width: 10.0),
-            Text('Score: ${player.getCardScore(card)}')
-          ],
-        ),
+      CardDetailTile cardDetailTile = CardDetailTile(
+        player: player,
+        card: card
       );
 
       if(iterator % 2 == 0) {
-        left.add(cardDetail);
+        left.add(cardDetailTile);
       } else {
-        right.add(cardDetail);
+        right.add(cardDetailTile);
       }
-      
+
       iterator++;
     }
-    
-    Row row = Row(
+
+    return Row(
       children: <Expanded>[
         Expanded(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: left,
           )
         ),
         Expanded(
           child: Column(
-            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
             children: right,
           )
         )
-      ]
+      ],
     );
-    
-    return row;
   }
 
   @override
